ea1cfe843b780cec461eabccb029bad7a5b1b49d
Jake Welch <jaketwelch@gmail.com>
2025-11-05 13:56:44 -0500

    Cinetizer tool update

--- src/data/tools.js
+++ src/data/tools.js
@@ -1,8 +1,36 @@
 export default [
   {
+    name: '↳ <a href="https://jake-welch-design.github.io/cinetiser-tool/" target="_blank">Cinetizer Tool</a>',
+    date: '2025',
+    type: 'Collage',
+    description:
+      'Inspired by the "Cinetizations" of Pol Bury, this tool takes an image and distorts it with a series of rotated radial cuts',
+    video: {
+      mp4: require('@/assets/videos/cinetizer-tool.mp4'),
+      webm: require('@/assets/videos/cinetizer-tool.webm'),
+    },
+    images: [
+      {
+        src: require('@/assets/images/cinetizer-tool/mockups/cinetizer-01.jpg'),
+        alt: 'Cinetization poster mockups',
+        caption: 'Cinetization poster mockups',
+      },
+      {
+        src: require('@/assets/images/cinetizer-tool/mockups/cinetizer-02.jpg'),
+        alt: 'Cinetization billboard mockup',
+        caption: 'Cinetization billboard mockup',
+      },
+      {
+        src: require('@/assets/images/cinetizer-tool/mockups/cinetizer-03.jpg'),
+        alt: 'Cinetization CD mockup',
+        caption: 'Cinetization CD mockup',
+      },
+    ],
+  },
+  {
     name: '↳ <a href="https://jake-welch-design.github.io/depth_collage_generator/" target="_blank">Depth Collage Generator</a>',
     date: '2025',
-    type: 'Image manipulation',
+    type: 'Collage',
     description:
       'Combines the pointcloud and depth map of two different images to create a layered 3D collage',
     video: {
@@ -30,7 +58,7 @@ export default [
   {
     name: '↳ <a href="https://jake-welch-design.github.io/BOOK_COVER_DESIGN_TOOL/" target="_blank">Grid Offset Tool</a>',
     date: '2024',
-    type: 'Image manipulation',
+    type: 'Collage',
     description:
       'Built for the design of <a href="https://www.instagram.com/ashreeder/" target="_blank">Ashton Reeder\'s</a> book <i>Fabricated Landscapes</i></a>, this tool takes an image input, divides it into a grid, and uses noise and wave algorithms to offset each of the cells. ',
     video: {
@@ -58,7 +86,7 @@ export default [
   {
     name: '↳ <a href="https://jake-welch-design.github.io/Glitch_type_generator_tool/" target="_blank">Type Runners Tool</a>',
     date: '2024',
-    type: 'Typography',
+    type: 'Typography manipulation',
     description:
       'Divides text into chunks that run in random directions disitorting the letterforms',
     video: {
@@ -86,7 +114,7 @@ export default [
   {
     name: '↳ <a href="https://jake-welch-design.github.io/gradient-flower-tool/" target="_blank">Gradient Flower Generator</a>',
     date: '2024',
-    type: 'Generative graphics',
+    type: 'Graphics',
     description:
       'Generates flower-like spiral motifs by rotating animated gradient slices',
     video: {
@@ -142,7 +170,7 @@ export default [
   {
     name: '↳ <a href="https://jake-welch-design.github.io/tile-collage-generator/" target="_blank">Tile Collage Tool</a>',
     date: '2024',
-    type: 'Image manipulation',
+    type: 'Collage',
     description:
       'Takes an image input, divides it into a grid, and randomly rotates each square by a 90 degree increments to form new compositions',
     video: {
@@ -170,7 +198,7 @@ export default [
   {
     name: '↳ <a href="https://hunteryoung.studio/i-am" target="_blank">I AM Virtual Mirror</a>',
     date: '2022',
-    type: 'Web art, interactive',
+    type: 'Web art',
     description:
       'Built for <a href="https://www.instagram.com/hunneryoung/" target="_blank">Hunter Young\'s</a> <a href="https://hunteryoung.studio/i-am-mirror" target="_blank">"I AM Mirror"</a> product release, this webpage accesses the webcam to create a virtual I AM mirror in browser.',
     // iframe: 'https://www.youtube.com/embed/9Rz9G7R42c8?si=ds7PbTliIyFoAS7s',
